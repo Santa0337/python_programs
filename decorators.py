5cc20546b1ba9ff9e7086853deaf77110b256bc0
Dhanush <r190337@rguktrkv.ac.in>
2025-08-20 06:19:22 +0000

    updating the decorator file

--- decorators.py
+++ decorators.py
@@ -1,13 +1,28 @@
-def decor(func):
-    def inner(name):
-        if name == 'dhanush':
-            print("Hello Dhanush Bad Morning")
-        else:
-            func(name)
-    return inner
-@decor
-def wish(name):
-    print("Hello",name,"Good Morning")
-wish('ravi')
-wish('john')
-wish('dhanush')
+# def decor(func):
+#     def inner(name):
+#         if name == 'dhanush':
+#             print("Hello Dhanush Bad Morning")
+#         else:
+#             func(name)
+#     return inner
+# @decor
+# def wish(name):
+#     print("Hello",name,"Good Morning")
+# wish('ravi')
+# wish('john')
+# wish('dhanush')
+def decor(func):   
+    def inner(name):   
+        if name=="Sunny":   
+            print("Hello Sunny Bad Morning")   
+        else:   
+            func(name)   
+    return inner   
+  
+def wish(name):   
+    print("Hello",name,"Good Morning")   
+decorfunction=decor(wish)       
+wish("Durga") #decorator won’t be executed   
+wish("Sunny")      
+decorfunction("Durga")#decorator will be executed   
+decorfunction("Sunny") 
